d0e1403e06949e60d7ea90036e32b6577c603c05
Sidhant Panda <sidhantpanda@gmail.com>
2014-04-20 19:02:10 +0530

    shifting code to implementation.py

--- mongo.py
+++ mongo.py
@@ -5,27 +5,7 @@ import pymongo
 from pymongo import MongoClient
 from collection import *
 
-listof = []
-listof = findall()
-print "Printing list of file names:"
-print listof
-
-#assign them ids
-ids=assignids(listof)
-print ''
-print "Printing id:filename pairs"
-print ids
-
-# def connect_to_db(name):
+# def connect_to_db(port,database):
 client = MongoClient('localhost', 27017)
 db = client['tfidf']
 collection = db['files']
-
-
-for data in ids:
-	collection.save(data)
-# print post_id
-print ''
-print collection.find_one({"1": "first.txt"})
-db.drop_collection(collection)
-# post_id
